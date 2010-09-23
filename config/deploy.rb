1ec80c015cf96973328f7cc3d8ab90297bb397d2
Brian <brian@insightify.com>
2010-09-23 10:44:40 -0700

    added content

---
+++ config/deploy.rb
@@ -0,0 +1,13 @@
+set :repository,  "git@github.com:insightmethods/sdbenicia.org.git"
+set :user,        "sites"
+set :domain,      "#{user}@insightmethods.com"
+set :deploy_to,   "/home/sites/sdbenicia.org"
+set :port,          4000
+set :merb_env,      "preview"
+set :processes,     1
+set :revision,    "origin/fixing"
+
+
+namespace :vlad do
+
+end
