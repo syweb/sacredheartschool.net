f83fe03244306a993fc09765d15b83d1d2e3f3ff
Brian <brian@insightify.com>
2010-09-23 12:59:31 -0700

    text corrections

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
-set :repository,  "git@github.com:insightmethods/sdbenicia.org.git"
+set :repository,  "git@github.com:insightmethods/sacredheartschool.net.git"
 set :user,        "sites"
 set :domain,      "#{user}@insightmethods.com"
-set :deploy_to,   "/home/sites/sdbenicia.org"
+set :deploy_to,   "/home/sites/sacredheartschool.net"
 set :port,          4000
 set :merb_env,      "preview"
 set :processes,     1
-set :revision,    "origin/fixing"
+set :revision,    "origin/master"
 
 
 namespace :vlad do
